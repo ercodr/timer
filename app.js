60ae700dfae08efebd71b58f7d10553bc1b8655b
ercodr <82173838+Dahcodingkid@users.noreply.github.com>
2021-09-06 20:07:56 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+let sec = document.getElementById('sec');
+let min = document.getElementById('min');
+let hr = document.getElementById('hr');
+
+const reset = document.getElementById('reset');
+const stop = document.getElementById('stop');
+
+// START TIMER BUTTON EVENT
+let startTimer = document.getElementById('start-timer');
+
+let intervalID;
+
+startTimer.addEventListener('click', () => {
+
+    intervalID = setInterval( () => {
+        sec.innerText ++;
+
+        if(sec.innerText == 60){
+            min.innerText ++;
+            sec.innerText = 0 ;
+        } else if(min.innerText == 60){
+            hr.innerText ++;
+            min.innerText = 0;
+        };
+
+    }, 1000);
+
+});
+
+
+reset.addEventListener('click', () => {
+    
+    clearInterval(intervalID);
+    min.innerText = '00';
+    sec.innerText = 0;
+    hr.innerText = '00';
+});
+
+stop.addEventListener('click', () => {
+    
+    clearInterval(intervalID);
+});
